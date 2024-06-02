f5e39bd616be5191425f552181581e81b1661a73
dreamik <dream-ik89@naver.com>
2024-06-02 21:36:24 +0900

    feat: Check for excess properties

--- src/forTypeScript/note/section03.md
+++ src/forTypeScript/note/section03.md
@@ -16,3 +16,53 @@
 - 최상위 unknown 바로 아래
 - `var` 처럼 제한되지 않는 타입은 위험한 타입
 - never 타입을 제외한 모든 타입과 캐스팅 가능
+
+# II. 객체 호환성
+## A. 기본 타입 호환성
+- 기본 타입과 리터럴 타입간 호환성
+  - 업캐스팅만 가능
+  - 더 구체적인(더 제한이 큰) 타입으로의 이동의 어려움
+```typescript
+let num1: number = 10;// number type
+let num2: 10 = 10;// number literal
+//super에 child가 둘어감: upper casting
+num1=num2;
+// num2 = num1; down casting x
+```
+## B. 객체 타입 호환성
+- 타입이 요구하는 속성(properties)을 근거로 타입 호환이 체크됨
+- Animal 타입에 Dog 타입을 넣을때 초과 프로퍼티는 무시됨
+```typescript
+type Animal = {
+    name: string;
+    color: string;
+};
+type Dog = {
+    name: string;
+    color: string;
+    bread: string;
+};
+let animal:Animal = {
+    name:"기린",
+    color:"yellow"
+}
+let dog:Dog={
+    name: "명명",
+    color: "brown",
+    bread: "진도"
+}
+animal = dog;//복사할때 필수 값을 만족하면 캐스팅 가능
+```
+- 직접 객체 생성시에는 타입스크립트의 초과 프로퍼티 체크에 걸림
+```typescript
+let dog:animal = {
+  name: "명명",
+  color: "brown",
+  // bread: "진도" 초과 프로퍼티 체크(in typescript)
+}
+```
+# III. 대수타입
+
+# IV. 타입 관련(추론, 단언, 좁히기)
+
+# V. 서로소 유니온
---
+++ src/forTypeScript/section3/src/chapter3.ts
@@ -0,0 +1,63 @@
+/**
+ * 기본 타입간 호환성
+ */
+let num1: number = 10;// number type
+let num2: 10 = 10;// number literal
+//super에 child가 둘어감: upper casting
+num1=num2;
+// num2 = num1; down casting x
+
+/**
+ * 객체 타입간의 호환성
+ * 구조적 타입 시스템으로 property 기준 체크
+ * 구체적일수록(속성이 많을 수록) 호환이 어렵다.
+ */
+type Animal = {
+    name: string;
+    color: string;
+};
+type Dog = {
+    name: string;
+    color: string;
+    bread: string;
+};
+let animal:Animal = {
+    name:"기린",
+    color:"yellow"
+}
+let dog:Dog={
+    name: "명명",
+    color: "brown",
+    bread: "진도"
+}
+animal = dog;//복사할때 필수 값을 만족하면 캐스팅 가능
+// dog = animal;//내부의 properties가 다를 경우 거절됨.
+// dog는 bread를 필수로 받는데 animal에 이 속성을 지니지 않으므로 구조를 만족시키지 못한다.
+
+type Book = {
+    title: string;
+    author: string;
+    price: number;
+}
+type ProgrammingBook = {
+    title: string;
+    author: string;
+    price: number;
+    skill: string;
+}
+let book:Book;
+let react:ProgrammingBook = {
+    title: "react 공부",
+    author: "홍길동",
+    price: 34_000,
+    skill: "react.js"
+}
+book = react;
+// react=book;
+
+let book2:Book = {
+    title: "react 공부",
+    author: "홍길동",
+    price: 34_000,
+    // skill: "react.js" 초과 프로퍼티 검사(typescript)
+}
