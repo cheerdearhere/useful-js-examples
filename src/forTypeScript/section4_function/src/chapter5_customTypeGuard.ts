d5ff1f2f3591d69c8de7d40ef4b61eabfb8c94ef
dreamik <dream-ik89@naver.com>
2024-06-04 19:25:09 +0900

    feat: custom type guard

---
+++ src/forTypeScript/section4_function/src/chapter5_customTypeGuard.ts
@@ -0,0 +1,25 @@
+
+type Dog = {
+    name: string;
+    isBark: boolean;
+}
+type Cat = {
+    name: string;
+    isScratch:boolean;
+}
+type Animal = Dog | Cat;
+
+function isDog(animal:Animal): animal is Dog{
+    return (animal as Dog).isBark !== undefined;
+}
+function isCat(animal:Animal): animal is Cat{
+    return (animal as Cat).isScratch !== undefined;
+}
+function waring(animal:Animal){
+    if(isDog(animal)){
+        console.log(animal.isBark);
+    }
+    else if(isCat(animal)){
+        console.log(animal.isScratch);
+    }
+}
